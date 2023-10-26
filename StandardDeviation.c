a480a4f30b00bbd578f167f4c4c2ec557d0b226f
rootkitpan <uwstoneblade@sina.com>
2023-10-26 23:59:18 +0800

    ddd

--- StandardDeviation.c
+++ StandardDeviation.c
@@ -18,3 +18,42 @@ double StandardDeviation(int32_t* array, uint32_t n)
 
 	return sd;
 }
+
+static uint32_t Old_N;
+static uint32_t Old_SumOfSquare;
+static double Old_Mean;
+
+void StartCalculateSD(void)
+{
+	_N = 0;
+	_SumOfSquare = 0;
+	_Mean = 0;
+}
+
+void AddOneSample(int32_t sample)
+{
+	
+}
+
+double GetSD()
+{
+
+}
+
+/*
+  n --> n+1
+
+  u(n)
+  u(n+1) = [ u(n) * n + an+1 ] / (n+1);
+ 
+  X(n) = (a1 - u)2 + (a2 - u)2 + ... + (an - u)2
+       = (a1)2 + (a2)2 + ... (an)2 + nu2 - 2(a1+a2+...+an)u
+	   = (a1)2 + (a2)2 + ... (an)2 - nu2
+	   = sum of square - sum2 / n
+ 
+  X(n+1) = (a1)2 + (a2)2 + ... (an)2 + (an+1)2 
+            - (n+1)[(nu + an+1)/(n+1)]2
+	    = (a1)2 + (a2)2 + ... (an)2 + (an+1)2 -
+		   (nu + an+1)2 / (n+1)
+ 
+ */
